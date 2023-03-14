6b0a97dac72acd0d592310e905bcf6e5aaba60d8
treasureszn <105241566+treasureszn@users.noreply.github.com>
2023-03-14 15:18:11 +0100

    made a few changes

--- index.js
+++ index.js
@@ -22,4 +22,19 @@ if (currentTheme) {
   }
 }
 
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
+document.addEventListener("scroll", function() {
+  var skillsSection = document.querySelector(".skills-section");
+  if (isElementInViewport(skillsSection)) {
+    skillsSection.classList.add("animate");
+  }
+});
